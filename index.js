10ac49bbe4935a1c9dc4787565b30a8bf1c666c5
High_Nitro <meraj1399god@gmail.com>
2025-10-16 00:57:10 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// index.js
-import fs from "fs";
-import fetch from "node-fetch";
-import FormData from "form-data";
-
+const fs = require("fs");
+const fetch = require("node-fetch");
 // ======= تنظیمات =======
 const TOKEN = "7346348218:AAGU8aUR-07GtJUZxw6JQ2X2Os9Jm_ZhGYU";
 const BASE_URL = `https://api.telegram.org/bot${TOKEN}`;
